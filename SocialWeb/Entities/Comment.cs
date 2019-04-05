9e7764f3de40e2dc0242eafffb2b10e85c63a0f9
Ezequiel <ezeq.uiel@hotmail.com>
2019-04-05 04:26:11 -0300

    Criado sistema que simula função básica de uma rede social

---
+++ SocialWeb/Entities/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SocialWeb.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+
+        public Comment()
+        {
+
+        }
+
+        public Comment (string text)
+        {
+            Text = text;
+        }
+    }
+}
